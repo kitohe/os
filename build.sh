88dd2d01f639f563bd0496b2a9c61d01923536da
Daniel Madrala <dmadrala@wp.pl>
2021-03-19 22:13:17 +0100

    makefile fixed

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ echo "Generating iso..."
 eval genisoimage -no-emul-boot -boot-load-size 4 -input-charset iso8859-1 -o bootloader.iso -b bootloader.img .
 
 cd ..
-echo "Cleaning..."
 eval make clean
 
 echo "Done"
